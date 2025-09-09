b625a17df2b73896258952cc3da0628821a590bd
Datntd <ndat84932gmail.com>
2025-09-09 09:05:29 +0700

    lần 1

---
+++ src/spc/edu/bai2helloword.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+/**
+ *
+ * @author Administrator
+ */
+public class bai2helloword {
+    public static void main(String[] args) {
+        System.out.println("lap trinh java");
+        /*
+        ghi chu
+        */
+    }
+}
